4cbd4b3e2c9bf5a8bcafe2e83370710e28a69033
Vojtech Jaeger <vojtajaeger@gmail.com>
2025-11-26 14:28:44 +0000

    Dokončeno

--- ukoly_z_hodin_python/prvni_priklad.py
+++ ukoly_z_hodin_python/prvni_priklad.py
@@ -1,3 +1,4 @@
+from os.path import join,realpath,dirname
 vyska:int = int(input("zadej vysku stromu: "))
 kmen_vyska:int = int(input("zadej vysku kmene: "))
 kmen_sirka:int = int(input("zadej sirku kmene: "))
@@ -8,6 +9,14 @@ for i in range(vyska):
 
 for i in range(kmen_vyska):
     print(" "*(vyska-((kmen_sirka)//2)-1)+"*"*(kmen_sirka))
-    
 
 
+with open("ukoly_z_hodin_c++/sprava-studentu/studenti_serazeno.txt","r",encoding="utf-8") as f:
+    print(f.read())
+
+cesta=join(dirname(realpath(__file__)),"ukoly_z_hodin_c++/sprava-studentu/studenti_serazeno.txt")
+with open(cesta,"r",encoding="utf-8") as f:
+    for c, line in enumerate(f):
+        for znak in line:
+            if znak.isdigit():print(znak,end=", ")
+            print()
