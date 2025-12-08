15bd5728faebdf183a595dd1e8036bc08573971e
Vojtech Jaeger <vojtajaeger@gmail.com>
2025-12-08 16:52:27 +0000

    Dokončeno

--- ZpetDoMinulosti/src/main.cpp
+++ ZpetDoMinulosti/src/main.cpp
@@ -1,23 +1,109 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <optional>
 #include "AudioManager.h"
+#include "MenuScreen.h"
+#include "SettingScreen.h"
+
+
+enum GameState
+{
+    MENU,
+    SETTINGS
+};
+
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({2560,1440}),L"Zpět do minulosti");
-    window.setFramerateLimit(165);
-    
-    while (window.isOpen())
+    sf::RenderWindow window(sf::VideoMode({800,600}),L"Zpět do minulosti");
+    window.setFramerateLimit(60);
+
+    AudioManager audio;
+    if(!audio.loadAssets())
     {
-        while (const std::optional event=window.pollEvent())
+        std::cerr << "Varovani: Nepodarilo se nacist zvuky" << std::endl;
+    }
+    audio.playMusic();
+
+    sf::Font font;
+    if(!font.openFromFile("assets/font.tff"))
+    {
+        if(!font.openFromFile("../assets/font.tff"))
         {
-            if(event->is<sf::Event::Closed>())
+            std::cerr << "CHYBA: Font nenalezen!" << std::endl;
+            return -1;
+        }
+    }
+
+    MenuScreen menu(800,600,font);
+    SettingsScreen settings(800,600,font);
+
+    GameState currentState=MENU;
+
+    bool mouseClickedReleased = true;
+
+    while(window.isOpen())
+    {
+        while(const std::optional event = window.pollEvent())
+        {
+            if(event -> is<sf::Event::Closed>())
             {
                 window.close();
             }
+
+            if(event -> is<sf::Event::MouseButtonReleased>())
+            {
+                mouseClickedReleased=true;
+            }
         }
 
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && mouseClickedReleased)
+        {
+            if (currentState == MENU)
+            {
+                int action = menu.handleInput(window);
+
+                if(action !=0)
+                {
+                    mouseClickedReleased= false;
+                    audio.PlayClick();
+                }
+                if(action ==1)
+                {
+                    std::cout << "Tlacitko START: Hra se spusti az dodelam GameScreen" << std::endl;
+                }
+                else if (action ==2)
+                {
+                    currentState= SETTINGS;
+                }
+                else if(action == 3)
+                {
+                    window.close();
+                }
+            }
+
+            else if(currentState = SETTINGS)
+            {
+                bool goBack= settings.handleInput(window,audio);
+                if(goBack)
+                {
+                    currentState = MENU;
+                    mouseClickedReleased = false;
+                    audio.PlayClick();
+                }
+            }
+        }
         window.clear(sf::Color(30,30,30));
+        if(currentState = MENU)
+        {
+            menu.draw(window);
+        }
+        else if(currentState = SETTINGS)
+        {
+            settings.draw(window);
+        }
         window.display();
     }
 
